02e8d59cb493ef17f50a16c2391b3cefc2549694
carinehuerbin <48594745+carinehuerbin@users.noreply.github.com>
2019-05-06 20:49:08 +0200

    starter map
    app with map, mouseover, mouseout
    and simple tooltip with municipalities names

--- app entwurf.js
+++ app entwurf.js
@@ -22,21 +22,97 @@ function drawMap() {
 
         dataset = data;
 
-
         var Gemeinden = topojson.feature(data, data.objects.municipalities).features;
-
-
+        
+        var Seen = topojson.feature(data, data.objects.lakes).features;   
+        
+        var Kantone = topojson.feature(data, data.objects.cantons).features; 
+        
+            
         var path = d3.geoPath()
             .projection(null);
+        
+        
+        div =
+            d3.select("body").append("div")
+            .attr("class", "tooltip")
+            .style("opacity", 0);
 
+
+        
         svg.append("g")
             .attr("class", "municipalities")
             .selectAll("path")
             .data(Gemeinden)
             .enter().append("path")
             .attr("id",function(d){return d.id})
-            .style("fill", "grey")    
-            .attr("d", path);
-    }
+            .style("fill", "WhiteSmoke")   
+            .style('stroke-width', '0.5')
+        
+        
+        
+        .on("mouseover", function (d) {
+            d3.select(this)
+            .style("fill", "Orange")
+            .style('stroke', "#000")
+            .style('stroke-width', function (d) {
+            return '1';
+          });
+            
+            
+            
+        div.transition()
+            .duration(200)
+            .style("opacity", 0.9);
+            
+        div
+            .html("<strong>" + d.properties.name + "</strong>")
+            .style("left", (d3.event.pageX) + "px")
+            .style("top", (d3.event.pageY-28) + "px");
+            })
+        
+        .on("mouseout", function (d) {
+            d3.select(this)
+              .style("fill", "WhiteSmoke")
+              .style('stroke', "#000")
+              .style('stroke-width', '0.5');
+            
+            
+        div.transition()
+            .duration(500)
+            .style("opacity", 0.9);
+        })
+	  
+		      .attr("d", path);
 
-    };
+        
+        svg.append("g")
+            .attr("class", "cantons")
+            .selectAll("path")
+            .data(Kantone)
+            .enter().append("path")
+            .attr("id",function(d){return d.id})
+            .style("fill", "none")
+            .style('stroke',"#000")
+            .style('stroke-width', '1')
+        
+            .attr("d", path);
+            
+        
+        svg.append("g")
+            .attr("class", "lakes")
+            .selectAll("path")
+            .data(Seen)
+            .enter().append("path")
+            .attr("id",function(d){return d.id})
+            .style("fill", "skyblue")
+            .style('stroke', "darkblue")
+    
+            .attr("d", path);
+         
+        
+
+        
+        
+}
+};
