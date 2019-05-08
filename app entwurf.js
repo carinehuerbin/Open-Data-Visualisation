c642d4d4064414ee37290b76eb6ce83b882022a6
carinehuerbin <48594745+carinehuerbin@users.noreply.github.com>
2019-05-08 22:42:29 +0200

    tried some stuff which did not work unfortunately
    see comments
    try to add donut chart to tooltip

--- app entwurf.js
+++ app entwurf.js
@@ -9,12 +9,14 @@ function drawMap() {
         var svg = d3.select("body").append("svg")
             .attr("width", width)
             .attr("height", height);
-
+    
 
         //load data files
         d3.queue()
             .defer(d3.json, "ch_withoutmunic.json")
+            /* .defer(d3.csv, "Geb_Klassen_ger.csv") */
             .await(ready)
+
 
     function ready (error, data){
 
@@ -23,12 +25,21 @@ function drawMap() {
         dataset = data;
 
         var Gemeinden = topojson.feature(data, data.objects.municipalities).features;
-        
+     /*   
+    Gemeinden.forEach(function (name){
+        Geb_Klassen_ger.some(function (csvrow){
+        if(name == csvrow.MunicipalityNumber) {
+            municipalities.properties.data2 = csvrow; 
+            return true;           
+                }    
+            });    
+        });
+        */ 
         var Seen = topojson.feature(data, data.objects.lakes).features;   
         
         var Kantone = topojson.feature(data, data.objects.cantons).features; 
         
-            
+        
         var path = d3.geoPath()
             .projection(null);
         
@@ -59,16 +70,22 @@ function drawMap() {
             return '1';
           });
             
+            /* bei Mouseover kommt eine Funktion rein mit der Anweisung, welche Daten mit dem Donut Chart angezeigt werden sollen: 
+             .on("click", function (d) {
+            d3.select('#piechart').select('h3').html(d.properties.data.GEMEINDE_NAME + " " + datumI); */
             
             
         div.transition()
             .duration(200)
             .style("opacity", 0.9);
+       
             
+            // hier im Tooltip den Donut Chart reinladen / verknüpfen via ID oder Gemeindename
         div
             .html("<strong>" + d.properties.name + "</strong>")
             .style("left", (d3.event.pageX) + "px")
             .style("top", (d3.event.pageY-28) + "px");
+            
             })
         
         .on("mouseout", function (d) {
